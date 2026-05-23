4e0b04e3b9e45a7e18f382d9a4fbc24c3b749cf8
knull-cc <knullcc@gmail.com>
2026-05-23 13:09:20 +0800

    first commit

--- run_main_crosslink.sh
+++ run_main_crosslink.sh
@@ -1,13 +1,1 @@
-sh scripts/GTR/etth1.sh;
-sh scripts/GTR/etth2.sh;
-sh scripts/GTR/ettm1.sh;
-sh scripts/GTR/ettm2.sh;
-sh scripts/GTR/weather.sh;
-sh scripts/GTR/electricity.sh;
-sh scripts/GTR/traffic.sh;
-sh scripts/GTR/solar.sh;
-# sh scripts/GTR/pems03.sh;
-# sh scripts/GTR/pems04.sh;
-# sh scripts/GTR/pems07.sh;
-# sh scripts/GTR/pems08.sh;
-
+sh scripts/crosslink/etth1.sh;
